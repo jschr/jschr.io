ac4536d1b9ff3a1bb491d7d4b488bef09f727e36
Jordan Schroter <jordan.schroter@gmail.com>
2017-04-10 17:11:35 -0400

    removes safari ui when added to ios homescreen

--- app/components/Template.tsx
+++ app/components/Template.tsx
@@ -17,6 +17,7 @@ export default function Template(props: TemplateProps) {
         <meta charSet='utf-8' />
         <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <link rel='shortcut icon' href={require('../assets/favicon.png')} />
         <title>{`${name} | ${description}`}</title>
         <style>{props.css}</style>
