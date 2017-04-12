a5d14839d09a40194c314360f1e24da6f63b69a1
Jordan Schroter <jordan.schroter@gmail.com>
2017-04-12 17:38:06 -0400

    app-package doesn't need to be an env var

--- infrastructure/env-dev/dev.tf
+++ infrastructure/env-dev/dev.tf
@@ -1,7 +1,6 @@
 variable "region"                       { }
 variable "aws_access_key"               { }
 variable "aws_secret_key"               { }
-variable "app_package"                  { }
 variable "domain"                       { }
 variable "twitter_consumer_key"         { }
 variable "twitter_consumer_secret"      { }
@@ -32,8 +31,8 @@ provider "mailgun" {
 module "app" {
   source = "../modules/app"
 
+  app_package                   = "../../build/app-package.zip"
   domain                        = "${var.domain}"
-  app_package                   = "${var.app_package}"
   twitter_consumer_key          = "${var.twitter_consumer_key}"
   twitter_consumer_secret       = "${var.twitter_consumer_secret}"
   twitter_access_token          = "${var.twitter_access_token}"
